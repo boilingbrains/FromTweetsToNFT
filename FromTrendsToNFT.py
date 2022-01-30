d86ea8bbeede1eb5de966ca84dde450bc3712ee9
Boilingbrains <68190528+boilingbrains@users.noreply.github.com>
2022-01-30 23:46:01 +0100

    use request methods and getuserid function

--- FromTrendsToNFT.py
+++ FromTrendsToNFT.py
@@ -1,33 +1,74 @@
-from logging import exception
 import os
-import tweepy
+import json
+import requests
 
-#Retreive keys and tokens
+# Retreive keys and tokens
 path = 'auth.txt'
 lines = []
 with open(path,"r") as f:
     lines =  f.readlines()
        
-consumer_key = lines[0][8:]
-consumer_secret = lines[1][14:]
-access_token = lines[2][12:]
-access_token_secret = lines[3][18:]
+bearer_token = lines[6][12:]
+
+def connect_to_endpoint(url, params):
+    response = requests.request("GET", url, auth=bearer_oauth, params=params)
+    print(response.status_code)
+    if response.status_code != 200:
+        raise Exception(
+            "Request returned an error: {} {}".format(
+                response.status_code, response.text
+            )
+        )
+    return response.json()
+
+def get_params():
+    # Tweet fields are adjustable.
+    # Options include:
+    # attachments, author_id, context_annotations,
+    # conversation_id, created_at, entities, geo, id,
+    # in_reply_to_user_id, lang, non_public_metrics, organic_metrics,
+    # possibly_sensitive, promoted_metrics, public_metrics, referenced_tweets,
+    # source, text, and withheld
+    return {"tweet.fields": "created_at"}
 
 
-#set up authentification
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+def bearer_oauth(r):
+    """
+    Method required by bearer token authentication.
+    """
 
-#initiate api object
-api = tweepy.API(auth)
+    r.headers["Authorization"] = f"Bearer {bearer_token}"
+    r.headers["User-Agent"] = "v2UserTweetsPython"
+    return r
 
-try:
-    api.verify_credentials()
-    print("Everythin works")
-except Exception as e:
-    print(e)
 
-# #test to retreive tweets from the timeline/feed?
-# public_tweets = api.home_timeline()
-# for tweet in public_tweets:
-#     print(tweet.text)
+def get_user_id(username):
+    getuid_url = "https://api.twitter.com/2/users/by?tweet.fields=id&usernames={}".format(username)
+    response = requests.request("GET", getuid_url, auth=bearer_oauth)
+    print(response.status_code)
+    if response.status_code != 200:
+        raise Exception(
+            "Request returned an error: {} {}".format(
+                response.status_code, response.text
+            )
+        )
+    return response.json()
+
+def create_url(user_id):
+    # Replace with user ID below
+    #user_id = 44196397 #elon musk
+    return "https://api.twitter.com/2/users/{}/tweets".format(user_id)
+
+
+
+
+def main():
+    #url = create_url()
+    #params = get_params()
+    #json_response = connect_to_endpoint(url, params)
+    #print(json.dumps(json_response, indent=4, sort_keys=True))
+    print(json.dumps(get_user_id("elonmusk"), indent=4, sort_keys=True))
+
+
+if __name__ == "__main__":
+    main()
